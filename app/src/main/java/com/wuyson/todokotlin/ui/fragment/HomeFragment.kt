8909bc378dbb14417965bdafaaf0ef0d960b0286
yangxiaoyu <y7206062@163.com>
2021-05-25 17:54:11 +0800

    RecyclerView settings

---
+++ app/src/main/java/com/wuyson/todokotlin/ui/fragment/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.wuyson.todokotlin.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.wuyson.common.base.BaseFragment
+
+class HomeFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+
+}
